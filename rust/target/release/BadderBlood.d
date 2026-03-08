92aae56575d6a4ccc67bb57bee02e35c2396f660
BitEU <45499599+BitEU@users.noreply.github.com>
2026-03-08 17:26:06 -0400

    Removed rust compilation folder

--- rust/target/release/BadderBlood.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Steven\Github\BadderBlood\rust\target\release\BadderBlood.exe: C:\Users\Steven\Github\BadderBlood\rust\src\main.rs
--- rust/target/release/deps/BadderBlood.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Steven\Github\BadderBlood\rust\target\release\deps\BadderBlood.d: src\main.rs
-
-C:\Users\Steven\Github\BadderBlood\rust\target\release\deps\BadderBlood.exe: src\main.rs
-
-src\main.rs:
